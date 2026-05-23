2d7cd0b55a4700b792044c9ffe0db4af7b88c458
অনিন্দিতা ও শোভনের প্রেমের পাঁচালী <aninditashobhan@gmail.com>
2026-05-24 01:34:07 +0530

    bbr

--- js/counter.js
+++ js/counter.js
@@ -1,10 +1,10 @@
 
-const startDate=new Date('2020-02-14');
+const startDate=new Date('2026-02-28');
 function updateCounter(){
  const now=new Date();
  const diff=now-startDate;
  const days=Math.floor(diff/(1000*60*60*24));
- const years=Math.floor(days/365);
- document.getElementById('counter').innerHTML=`❤️ ${years} Years Together<br>${days} Days of Memories`;
+ const months=Math.floor(days/30);
+ document.getElementById('counter').innerHTML=`❤️ ${months} months Together<br>${days} Days of Memories`;
 }
 updateCounter();
